749be8f62b3185570bc85b4551929e39ae6f155e
dcoimbra <david.coimbra@tecnico.ulisboa.pt>
2021-05-31 18:14:00 +0100

    deleted useless files

--- code2vec/evaluate_model.sh
+++
@@ -1,4 +0,0 @@
-type=path-injection-balanced-disjoint
-dataset_name=path-injection-balanced-disjoint
-
-python code2vec.py --load models/${type}/saved_model --test data/${type}/${dataset_name}.test.c2v
--- code2vec/examine_model.sh
+++
@@ -1,4 +0,0 @@
-type=path-injection-ground-truth-balanced
-dataset_name=path-injection-ground-truth-balanced
-
-python code2vec.py --load models/${type}/saved_model --predict
